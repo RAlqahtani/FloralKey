a47a4a4d4a3f57bb4962093dc5d991f2be2aee67
Raghad Al Qahtani <102378431+RAlqahtani@users.noreply.github.com>
2023-05-07 10:45:20 +0300

    FloralKey

---
+++ images/flowerWebsite/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> FloralKey.com </title>
+    <link rel="html" href="C:\Users\ragha\Desktop\flower website\html.html"> 
+
+</head>
+<body>
+<script type = "text/javascript">
+    alert ( " Welcome to Floral Key ! " )
+</script>
+</body>
+
+</html>
+
+
---
+++ php.php
@@ -0,0 +1,17 @@
+<?php
+echo "<html>
+<head>
+    <title> FloralKey.com </title>
+    <link rel="html" href='C:\Users\ragha\Desktop\flower website\html.html'> 
+
+</head>
+<body>
+<script type = 'text/javascript'>
+    alert ( 'Hi, Welcome in Floral Key ! ' )
+</script>
+</body>
+
+</html>";
+?>
+
+
